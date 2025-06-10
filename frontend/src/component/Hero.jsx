1d767e6a44ae07b8dc63e07dce4c991d86dc2c86
Ritesh7459 <140267718+Ritesh7m@users.noreply.github.com>
2025-06-11 00:31:39 +0530

    Update Hero.jsx

--- frontend/src/component/Hero.jsx
+++ frontend/src/component/Hero.jsx
@@ -21,20 +21,20 @@ const Hero = () => {
         {/* Left Section */}
         <div className="w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0">
           <div className="text-[#23066d]">
-            <h1 className="text-5xl">Welcome to Rs-Enterprises</h1>
+            <h1 className="text-5xl  text-white ">Welcome to Rs-Enterprises</h1>
             <br />
             <div className="flex items-center gap-2">
-              <p className="w-8 md:w-11 h-[2px] bg-[#23066d]"></p>
-              <p className="font-medium text-sm md:text-base">
+              <p className="w-8 md:w-11 h-[2px] bg-white"></p>
+              <p className="font-medium text-sm md:text-base  text-white">
                 OUR BESTSELLERS
               </p>
             </div>
-            <h1 className="text-3xl sm:py-3 lg:text-5xl leading-relaxed">
+            <h1 className="text-3xl sm:py-3 lg:text-5xl leading-relaxed  text-white">
               Latest Arrivals
             </h1>
             <div className="flex items-center gap-2">
-              <p className="font-semibold text-sm md:text-base">SHOP NOW</p>
-              <p className="w-8 md:w-11 h-[2px] bg-[#23066d]"></p>
+              <p className="font-semibold text-sm md:text-base  text-white">SHOP NOW</p>
+              <p className="w-8 md:w-11 h-[2px] bg-white"></p>
             </div>
           </div>
         </div>
